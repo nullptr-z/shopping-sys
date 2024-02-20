bc42c87b7b67abc4216dd51b8989e6a5cb7a6dfa
zhouzheng <zhouzheng0646@qq.com>
2024-02-20 23:12:03 +0800

    getuserlist

--- api-http/user-web/api/user.go
+++ api-http/user-web/api/user.go
@@ -1,17 +1,43 @@
 package api
 
 import (
+	"api-http/user-web/proto"
+	. "api-http/user-web/utils"
+	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func HandlerGetList(g *gin.Context) {
-	// 获取参数
-	param := g.PostForm("")
-	fmt.Println("param:", param)
-	// 参数校验
+	// zap.S().Debug("get user list")
+	// 链接 grpc 服务
+	ip, port := viper.Get("rpc.ip"), viper.Get("rpc.port")
+	rpcAddress := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := grpc.Dial(rpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		zap.S().Errorw("[HandlerGetList] connect rpc server of user failed", "msg", err.Error())
+	}
+	// 获取请求参数
+	page, _ := strconv.Atoi(g.DefaultQuery("page", "1"))
+	pageSize, _ := strconv.Atoi(g.DefaultQuery("pageSize", "5"))
+	// 创建 grpc client
+	client := proto.NewUserClient(conn)
+	rsp, err := client.GetUserList(
+		context.Background(),
+		&proto.PageInfo{Page: uint32(page), PageSize: uint32(pageSize)},
+	)
+	if err != nil {
+		zap.S().Errorw("[GetUserList] error", err.Error())
+		ResponseError(g, CodeInvalidParams, "code", GRPCStatusToHTTP(err))
+		return
+	}
 	// 响应结果
-	g.JSON(http.StatusOK, gin.H{"message": ""})
+	g.JSON(http.StatusOK, gin.H{"total": rsp.Total, "data": rsp.Data})
 }
