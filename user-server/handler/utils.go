3dcd246ab7c5b8e0dc83f954ae99f6a021ea32f4
zhouzheng <zhouzheng0646@qq.com>
2024-02-20 17:56:54 +0800

    user-server

---
+++ user-server/handler/utils.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"shopping-sys/user_server/model"
+	"shopping-sys/user_server/proto"
+)
+
+// 数据库查询的 user 数据转换到 proto.UserInfoResponse
+func IntoDbUseUserInfoResponse(u model.User) *proto.UserInfoResponse {
+	var user = proto.UserInfoResponse{
+		Id:       u.ID,
+		Mobile:   u.Mobile,
+		Password: u.Password,
+		NickName: u.NickName,
+		Gender:   u.Gender,
+		Role:     int32(u.Role),
+	}
+	if u.Birthday != nil {
+		user.Birthday = uint64(u.Birthday.Unix())
+	}
+	return &user
+}
